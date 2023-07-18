1c6ac0d2205064a39f5d6c499e3752d197d76426
suwara <35873168+suwara@users.noreply.github.com>
2023-07-18 14:26:25 +0700

    Update Main.java

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -2,11 +2,9 @@ package atm;
 
 public class Main {
     public static void main(String[] args) {
-        DataSourceDB dataSourceDB = new DataSourceDB();
-        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
-        Bank bank = new Bank("My Bank", dataSourceFile);
-        ATM atm = new ATM(bank);
-        AtmUI atmUI = new AtmUI(atm);
+         ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+        AtmUI atmUI = context.getBean(AtmUI.class);
         atmUI.run();
     }
 }
